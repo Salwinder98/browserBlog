5c03f31cc0e9b9d506cc35dfe10b3dcfedad6edb
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-26 14:46:04 -0400

    Form

--- js/index.js
+++ js/index.js
@@ -124,6 +124,31 @@ body {
 	
 </div>
 
+<------Form----->
+<div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
+
 <div id="footer">
 	<p id="copyright">&copy; Chris Spooner / SpoonGraphics (Please don’t steal my work)</p>
 	<p id="back-top"><a href="#">Going up?</a></p>
