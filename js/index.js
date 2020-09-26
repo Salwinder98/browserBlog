2ab8c0e0fc6c7c6cd0639da01b9d77c30471b93e
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-26 14:09:30 -0400

    Port1

--- js/index.js
+++ js/index.js
@@ -103,7 +103,7 @@ body {
 	<p>I’m forever creating design work for both myself as personal projects and as a hired gun for clients from around the world. Here’s a few of my most recent works.</p>
 	
 	<div class="portfolio-item">
-		<a href="#"><img src="Images/Poart1.png" alt="View more info" /></a>
+		<a href="#"><img src="Images/Port1.png" alt="View more info" /></a>
 		<p class="btn"><a href="#">See more</a></p>
 	</div>
 	
