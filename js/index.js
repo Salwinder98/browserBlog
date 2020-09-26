c8d4fdc9d3bb0e43bae972bf5bff0890995b9834
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-26 15:16:30 -0400

    Div change

--- js/index.js
+++ js/index.js
@@ -90,6 +90,9 @@ class Nav extends Page{
             <li><a href="portfolio.html">Portfolio</a></li>
             <li><a href="contact.html">Contact</a></li>
             </ul>
+            </div>
+
+            
             <div id="header">
 	<h1>Hello, I'm Salwinder Kaur.</h1>
 	<h2>I craft websites that are beautiful on both the inside and out.</h2>
@@ -104,11 +107,7 @@ body {
 }
 
 </style>
-            <h1>Hello, I'm Chris Spooner.</h1>
-            <h2>I craft websites that are beautiful on both the inside and out.</h2>
             
-            <p class="btn"><a href="portfolio.html">View my portfolio</a></p>
-        </div>
         <div id="content">
 	<h3>About Chris Spooner</h3>
 	
