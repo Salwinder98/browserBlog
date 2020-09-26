25cba7d9e601671fc63f552c6f57f9633dcb92cf
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-26 11:58:15 -0400

    remove items

--- js/index.js
+++ js/index.js
@@ -71,34 +71,8 @@ class Nav extends Page{
         }
 
         $("nav").html(`
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#home">Portfolio of ${sName}</a>
-        </div>
-     
-
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="portfolio.html">Portfolio</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            </ul>
-            <div id="header">
-	<h1>Hello, I'm Salwinder Kaur.</h1>
-	<h2>I craft websites that are beautiful on both the inside and out.</h2>
-	
-	<p class="btn"><a href="portfolio.html">View my portfolio</a></p>
-</div>
-        </div>
-
-    </div>
+       
+        
 
         `);
     }
