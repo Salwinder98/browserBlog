c59050f77154f8b00fd1ecbec11ab8ee9bbc10a8
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-27 01:40:21 -0400

    Port2

--- js/index.js
+++ js/index.js
@@ -116,7 +116,7 @@ class Nav extends Page{
 	</div>
 	
 	<div class="portfolio-item">
-		<a href="#"><img src="images/portfolio-2.jpg" alt="View more info" /></a>
+		<a href="#"><img src="Images/Port2.jpg" alt="View more info" /></a>
 		<p class="btn"><a href="#">See more</a></p>
 	</div>
 	
