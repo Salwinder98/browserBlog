c477fee2b3da9ad86ebe8ae62e6c73318f3913f5
Salwinder98 <Salwinder1234@yahoo.com>
2020-09-27 01:19:32 -0400

    Salwinder

--- js/index.js
+++ js/index.js
@@ -101,7 +101,7 @@ class Nav extends Page{
         
         
         <div id="content">
-	<h3>About Chris Spooner</h3>
+	<h3>About Salwinder kaur</h3>
 	
 	<p>I earn a living by creating custom brands and logo designs from scratch, as well as designing and building high quality websites and blogs, but I also enjoy producing the odd t-shirt graphic, illustration or character design. I pride myself in having the nerdy skills to build top notch creations online, as well as being knowledgeable in the print side of design.</p>
 	
